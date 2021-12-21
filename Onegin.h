d86a096d68b4626fddeb16466797981943c6ddf2
error404cyber <67262189+error404cyber@users.noreply.github.com>
2021-12-21 23:43:57 +0300

    library

--- Onegin.h
+++ Onegin.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+int compare(const void *first, const void* second);
 size_t count_fsize(FILE *f);
 size_t format_buffer(char *buf);
 void build_strings_formatted_buf(char *buf, char **strings, size_t count_lines);
