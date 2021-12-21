d6e319cb08fa17d1ec07053e580ac98232076f58
error404cyber <67262189+error404cyber@users.noreply.github.com>
2021-12-21 23:16:09 +0300

    all unit

---
+++ unit.c
@@ -0,0 +1,19 @@
+#include "unit.h"
+#include "Onegin.h"
+#include <assert.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int run_all_test(){
+    assert(test_mystrcmp() == OK);
+    assert(test_compar() == OK);
+    assert(test_count_lines() == OK);
+    assert(test_devide_lines() == OK);
+    return 0;
+}
+
+Error test_compare()
+  
+  
+Error test_
