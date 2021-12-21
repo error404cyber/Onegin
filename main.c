567fe761e571cca480823c7d38347579f7bdf095
error404cyber <67262189+error404cyber@users.noreply.github.com>
2021-12-21 15:26:50 +0300

    main

---
+++ main.c
@@ -0,0 +1,42 @@
+
+/**
+ * дается файл со строками
+ * нужно упорядочить строки в лексикографическом порядке
+ * то есть по алфавиту 
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "Onegin.h"
+
+int compare(const void *first, const void* second){
+  return strcmp(*(char**)first, *(char**)second);
+}
+
+int main(){
+FILE *input = fopen("Onegin.txt", "rb");
+//считывание файла
+size_t fsize = count_fsize(input);
+//создание буфера
+char* text = (char*)calloc(fsize, sizeof(char));
+fread(text, sizeof(char), fsize, input);
+
+size_t count_lines = format_buffer(text);
+
+
+char ** strings = (char**)calloc(count_lines, sizeof(char*));
+build_strings_formatted_buf(text, strings, count_lines);
+
+//сортировка массива строк
+qsort(strings, count_lines, sizeof(char**), compare);
+
+//вывод отсортированного буфера
+for(size_t i = 0; i<count_lines; ++i){
+  printf("%s\n", strings[i]);
+}
+
+
+return 0;
+}
