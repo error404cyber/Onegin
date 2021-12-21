0a801ca7c379886ca95004a698ea2cfba85681a5
error404cyber <67262189+error404cyber@users.noreply.github.com>
2021-12-21 22:47:29 +0300

    library unit

---
+++ unit.h
@@ -0,0 +1,8 @@
+#include "string.h"
+  
+ typedef enum Error { OK, NOT_OK } Error;
+
+void run_all_tests();
+Error test_format_buffer();
+Error test_count_lines();
+Error test_build_strings_formatted_buf();
