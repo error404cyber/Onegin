84416f31dc79ab7094d618b5fefad5322e1f8056
error404cyber <67262189+error404cyber@users.noreply.github.com>
2021-12-21 23:18:36 +0300

    library

--- unit.h
+++ unit.h
@@ -3,6 +3,7 @@
  typedef enum Error { OK, NOT_OK } Error;
 
 void run_all_tests();
+Error test_compare();
 Error test_format_buffer();
 Error test_count_lines();
 Error test_build_strings_formatted_buf();
