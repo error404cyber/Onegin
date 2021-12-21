b04dba5d6d9b7298017e5ca3d5ea7ca64534e857
error404cyber <67262189+error404cyber@users.noreply.github.com>
2021-12-22 00:39:23 +0300

    library unit

--- unit.c
+++ unit.c
@@ -8,7 +8,7 @@
 int run_all_test(){
     assert(test_compare() == OK);
     assert(test_format_buffer() == OK);
-    assert(test_count_lines() == OK);
+    assert(test_count_fsize() == OK);
     assert(test_build_strings_formatted_buf() == OK);
     return 0;
 }
@@ -27,7 +27,34 @@ Error test_compare(){
 Error test_fsize(){
     FILE *input = fopen("Onegin.txt", "rb");
     size_t fsize = count_fsize(input);
-    if (fsize!=7){
+    if (fsize!=5){
+        printf("Not right\n");
+        return NOT_OK;
+    }
+    return OK;
+}
+
+Error test_format_buffer(){
+    FILE *input = fopen("Onegin.txt", "rb");
+
+    size_t fsize = count_fsize(input);
+    char* text = (char*)calloc(fsize, sizeof(char));
+    fread(text, sizeof(char), fsize, input);
+    if (format_buffer(text)!=3) {
+        printf("Not right\n");
+        return NOT_OK;
+    }
+    return OK;
+}
+
+Error test_build_strings_formatted_buf(){
+    FILE *input = fopen("Onegin.txt", "rb");
+
+    size_t fsize = count_fsize(input);
+    char* text = (char*)calloc(fsize, sizeof(char));
+    fread(text, sizeof(char), fsize, input);
+    format_buffer(text);
+    if ((text[0] != '1') || (text[2] != '2') || (text[4] != '5')) {
         printf("Not right\n");
         return NOT_OK;
     }
